6f12bc6366c94df10a2f0b3ae2f3e43e9e5d09ef
TyrfingX <saschamueller@gmx.net>
2018-09-08 10:59:20 +0200

    Initial commit

---
+++ android/src/com/ascending/games/id1/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ascending.games.id1;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ascending.games.id1.DungeonCreatorGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DungeonCreatorGame(), config);
+	}
+}
---
+++ desktop/src/com/ascending/games/id1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ascending.games.id1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ascending.games.id1.DungeonCreatorGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DungeonCreatorGame(), config);
+	}
+}
