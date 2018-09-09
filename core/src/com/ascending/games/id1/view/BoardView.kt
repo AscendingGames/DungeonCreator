4c2d884649bb16870023e0608881fa3cfe624cfd
AscendingGames <ascending.games@hotmail.com>
2018-09-09 05:03:25 +0200

    Add IOS support
    Added IOS support

---
+++ core/src/com/ascending/games/id1/view/BoardView.kt
@@ -0,0 +1,12 @@
+package com.ascending.games.id1.view
+
+import com.ascending.games.id1.model.board.Board
+import com.ascending.games.lib.view.IView
+import com.badlogic.gdx.math.Rectangle
+
+class BoardView(val board : Board) : IView {
+    override fun render() {
+
+    }
+
+}
