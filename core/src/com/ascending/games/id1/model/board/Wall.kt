6d3338788c8cd659ca9070405a781b76e5b9bc61
AscendingGames <ascending.games@hotmail.com>
2018-09-30 15:58:13 +0200

    Removed magic number

--- core/src/com/ascending/games/id1/model/board/Wall.kt
+++ core/src/com/ascending/games/id1/model/board/Wall.kt
@@ -6,20 +6,25 @@ import com.ascending.games.lib.model.geometry.IRectangle2
 import com.badlogic.gdx.math.Vector2
 
 data class Wall(val roomElement : RoomElement, var direction : Direction4, var wallState : WallState) : IRectangle2, IHierarchical2 {
+
+    companion object {
+        const val WALL_SIZE = 0.1f
+    }
+
     override val relativePosition: Vector2
         get() = when (direction) {
             Direction4.LEFT -> Vector2(0f, 0f)
-            Direction4.RIGHT -> Vector2(0.9f, 0f)
             Direction4.DOWN -> Vector2(0f, 0f)
-            Direction4.UP -> Vector2(0f, 0.9f)
+            Direction4.RIGHT -> Vector2(1f - WALL_SIZE, 0f)
+            Direction4.UP -> Vector2(0f, 1f - WALL_SIZE)
         }
     override val parent: Any?
         get() = roomElement
     override val size: Vector2
         get() = when (direction) {
-            Direction4.LEFT -> Vector2(0.1f, 1f)
-            Direction4.RIGHT -> Vector2(0.1f, 1f)
-            Direction4.DOWN -> Vector2(1f, 0.1f)
-            Direction4.UP -> Vector2(1f, 0.1f)
+            Direction4.LEFT -> Vector2(WALL_SIZE, 1f)
+            Direction4.RIGHT -> Vector2(WALL_SIZE, 1f)
+            Direction4.DOWN -> Vector2(1f, WALL_SIZE)
+            Direction4.UP -> Vector2(1f, WALL_SIZE)
         }
 }
