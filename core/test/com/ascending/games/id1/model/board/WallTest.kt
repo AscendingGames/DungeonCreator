f7f3b71519c3a8858f01164b0f673e5f9b4da7ca
AscendingGames <ascending.games@hotmail.com>
2018-09-30 18:13:20 +0200

    Improved test coverage

--- core/test/com/ascending/games/id1/model/board/WallTest.kt
+++ core/test/com/ascending/games/id1/model/board/WallTest.kt
@@ -2,27 +2,35 @@ package com.ascending.games.id1.model.board
 
 import com.ascending.games.lib.model.geometry.Coord2
 import com.ascending.games.lib.model.geometry.Direction4
-import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Rectangle
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class WallTest {
 
     private val roomElement = RoomElement(Coord2.ZERO)
-    private val wall = Wall(roomElement, Direction4.DOWN, WallState.CLOSED)
+
 
     @Test
     fun getRelativePosition() {
-        assertEquals(Vector2.Zero, wall.relativePosition)
+        for (direction in Direction4.values()) {
+            val wall = Wall(roomElement, direction, WallState.CLOSED)
+            assertTrue(Rectangle(0f, 0f, 1f, 1f).contains(wall.relativePosition))
+        }
     }
 
     @Test
     fun getParent() {
+        val wall = Wall(roomElement, Direction4.DOWN, WallState.CLOSED)
         assertEquals(roomElement, wall.parent)
     }
 
     @Test
     fun getSize() {
-        assertEquals(Vector2(1f, Wall.WALL_SIZE), wall.size)
+        for (direction in Direction4.values()) {
+            val wall = Wall(roomElement, direction, WallState.CLOSED)
+            assertTrue(Rectangle(0f, 0f, 1f, 1f).contains(wall.size))
+        }
     }
 }
